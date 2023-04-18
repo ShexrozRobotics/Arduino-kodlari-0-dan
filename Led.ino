0f8fd6fd42da6714086ffbdad8eefab6807ece51
Shexroz Dehqonov <129843759+ShexrozRobotics@users.noreply.github.com>
2023-04-19 04:36:44 +0500

    comment
    Izohlar kodni tushunishga yordam beradi

--- Led.ino
+++ Led.ino
@@ -1,12 +1,12 @@
-int led = 13;
+int led = 13;  // O'zgaruvchi yaratib oldik, qiymatini 13 ga tenglashtiramiz
 
 void setup(){
-  pinMode(led,OUTPUT);
+  pinMode(led,OUTPUT);  // Led - 13 pinga ma'lumot chiqaramiz deb e'lon qilamiz
 }
 
 void loop(){
-  digitalWrite(led,HIGH);
-  delay(1000);
-  digitalWrite(led,LOW);
-  delay(1000);
+  digitalWrite(led,HIGH);   // Raqamli Led ga HIGH-(yoqilsin) deb buyruq berilgan
+  delay(1000);              // 1 sekund kutish
+  digitalWrite(led,LOW);    // Raqamli Led ga LOW-(o'chirilsin) deb buyruq berilgan
+  delay(1000);              // 1 sekund kutish
 }
