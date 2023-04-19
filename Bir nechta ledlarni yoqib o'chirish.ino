5654bacf0b659dbee021375d99cda834e5f97f36
Shexroz Dehqonov <129843759+ShexrozRobotics@users.noreply.github.com>
2023-04-19 05:01:16 +0500

    comment
    Izohlar kodni tushunishga yordam beradi

--- Bir nechta ledlarni yoqib o'chirish.ino
+++ Bir nechta ledlarni yoqib o'chirish.ino
@@ -1,22 +1,22 @@
-int led_1 = 13;
-int led_2 = 12;
-int led_3 = 11;
+int led_1 = 13; //Birinchi led chiroqni 13 - pinga ulaymiz
+int led_2 = 12; //Ikkinchi led chiroqni 12 - pinga ulaymiz
+int led_3 = 11; //Uchinchi led chiroqni 11 - pinga ulaymiz
 
 void setup(){
-  pinMode(led_1,OUTPUT);
-  pinMode(led_2,OUTPUT);
-  pinMode(led_3,OUTPUT);
+  pinMode(led_1,OUTPUT);  // Birinchi ledni OUTPUT deb e'lon qilamiz
+  pinMode(led_2,OUTPUT);  // Ikkinchi ledni OUTPUT deb e'lon qilamiz
+  pinMode(led_3,OUTPUT);  // Uchinchi ledni OUTPUT deb e'lon qilamiz
 }
 
 void loop(){
-  digitalWrite(led_1,1);
-  delay(1000);
-  digitalWrite(led_1,0);
-  digitalWrite(led_2,1);
-  delay(1000);
-  digitalWrite(led_2,0);
-  digitalWrite(led_3,1);
-  delay(1000);
-  digitalWrite(led_3,0);
-  delay(1000);
+  digitalWrite(led_1,1);  // Birinchi led ni yoqamiz (HIGH yoki 1 | yoqilsin)
+  delay(1000);            // 1 sekund kutib turish
+  digitalWrite(led_1,0);  // Birinchi led ni o'chiramiz
+  digitalWrite(led_2,1);  // Ikkinchi led ni yoqamiz
+  delay(1000);            // 1 sekund kutib turish
+  digitalWrite(led_2,0);  // Ikkinchi led ni o'chirish
+  digitalWrite(led_3,1);  // Uchinchi led ni yoqish
+  delay(1000);            // 1 sekund kutish
+  digitalWrite(led_3,0);  // Uchinchi led ni o'chirish
+  delay(1000);            // 1 sekund kutib turish
 }
